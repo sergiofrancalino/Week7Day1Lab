dc005da4288ac8e86f8e438557a4571c2a94e021
sergiofrancalino <sergio.francalino@tjac.jus.br>
2022-11-28 19:28:50 -0300

    Intaration 0

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-//ARQUIVO PRINCIPAL
+//Interation 0 - Server is live
 import express from "express";
 import * as dotenv from "dotenv";
 // import { uuid } from "uuidv4";
 
-//habilitar o servidor a ter variáveis de ambiente
+//ENABLING THE SERVER (environment variables)
 dotenv.config();
 
-//instanciar a variável que vai ficar responsável pelo nosso servidor -> app
+//INSTANTIATE the variable responsible for server -> App
 const app = express();
 
-//configurar o servidor para aceitar enviar e receber arquivos em JSON
+//CONFIGURE server to sending and receiving files in JSON 
 app.use(express.json());
 
-//banco de dados
+//Database 
 const bancoDados = [
   {
     id: "ee5999d7-02e9-4b3d-a1ab-f067eef54173",
@@ -26,7 +26,7 @@ const bancoDados = [
   },
 ];
 
-//CRIAÇÃO DAS ROTAS
+//ROUTES CREATION - Intaration 1
 //GET - all
 app.get("/all", (req, res) => {
   return res.status(200).json(bancoDados);
